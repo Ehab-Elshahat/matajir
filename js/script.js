a7a7f2f4cd975c1c459b93d59a4c2e1998de09d4
Ehab Elshahat <61202200+Ehab-Elshahat@users.noreply.github.com>
2023-09-03 11:33:39 +0300

    edit 2

--- js/script.js
+++ js/script.js
@@ -13,7 +13,6 @@ searchInput.addEventListener("blur", () => {
 // Show Hide Password Field
 let passwordField = document.querySelectorAll(".password input");
 let showHideIcon = document.querySelectorAll(".password .show-password");
-let mobileMenu = document.querySelector(".mobile-menu");
 showHideIcon.forEach((icon) => {
   icon.addEventListener("click", () => {
     let input = icon.previousElementSibling;
@@ -26,6 +25,7 @@ showHideIcon.forEach((icon) => {
 });
 
 // Show hide Menu in Mobile
+let mobileMenu = document.querySelector(".mobile-menu");
 let mobilTogglerMenu = document.querySelector(".custom-toggler");
 let overLay = document.querySelector(".overlay");
 mobilTogglerMenu.addEventListener("click", (e) => {
@@ -39,44 +39,43 @@ mobilTogglerMenu.addEventListener("click", (e) => {
   }
 });
 
-// Hide All Category Links when mouse leaves 
-let allCategoryBtn = document.querySelector(".all-categories .dropdown-toggle");
+// Close Menu When Clicking Escape key
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    if (mobileMenu.classList.contains("open")) {
+      mobileMenu.classList.remove("open");
+      mobilTogglerMenu.classList.remove("close");
+      overLay.classList.remove("show");
+    }
+  }
+});
 
-let allDropdown = document.querySelectorAll(".category li .dropdown-menu");
-let categoryLinks = document.querySelector(
-  ".categories-box .all-categories .dropdown-menu"
-);
-if (categoryLinks) {
-  categoryLinks.addEventListener("mouseleave", () => {
-    categoryLinks.classList.remove("show");
-    allCategoryBtn.classList.remove("show");
-  });
-}
-window.addEventListener("scroll", () => {
-  categoryLinks.classList.remove("show");
-  allCategoryBtn.classList.remove("show");
-  allDropdown.forEach((menu, index) => {
-    menu.classList.remove("show");
-  });
-})
+// Close Menu When Clicking Outside it
+window.addEventListener("click", (e) => {
+  // e.preventDefault()
+  console.log(e.target.classList);
+  if (
+    !e.target.classList.contains("custom-toggler") &&
+    !e.target.classList.contains("accordion-button") &&
+    !e.target.classList.contains("mobile-menu") &&
+    !e.target.classList.contains("link ")
+  ) {
+    mobileMenu.classList.remove("open");
+    mobilTogglerMenu.classList.remove("close");
+    overLay.classList.remove("show");
+  }
+});
 
-if (allDropdown) {
-  allDropdown.forEach((menu, index) => {
-    menu.addEventListener("mouseleave", () => {
-      menu.classList.remove("show");
-    });
-  })
-}
-// Handle Active Class On Links
+// Handle Active Class On Category Links
 let allCategoryLinks = document.querySelectorAll(
   ".category li .dropdown-toggle "
 );
-allCategoryLinks.forEach(link =>{
-  link.addEventListener("click", () =>{
-    allCategoryLinks.forEach(el=> el.classList.remove("active"))
+allCategoryLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    allCategoryLinks.forEach((el) => el.classList.remove("active"));
     link.classList.add("active");
-  })
-})
+  });
+});
 
 // Product page
 // Change Product Photo
@@ -123,8 +122,7 @@ if (increaseBtns) {
         decreaseBtns[index].classList.remove("active");
       }
     });
-  })
-
+  });
 }
 if (decreaseBtns) {
   decreaseBtns.forEach((btn, index) => {
@@ -139,13 +137,12 @@ if (decreaseBtns) {
         decreaseBtns[index].classList.remove("active");
       }
     });
-  })
-
+  });
 }
-// ShoppingCart Page 
+// ShoppingCart Page
 let itemsPrice = document.querySelectorAll(".cart-summary .price span");
 let sumPrice = document.querySelector(".sum-value span");
-let price = 0
+let price = 0;
 if (itemsPrice) {
   itemsPrice.forEach((item) => {
     price = price + Number(item.textContent);
@@ -162,8 +159,7 @@ if (addNewAddressBtn) {
       addNewAddressModel[index].classList.add("show");
       overLay.classList.add("show");
     });
-  })
-
+  });
 }
 if (closeModel) {
   closeModel.forEach((btn, index) => {
@@ -171,27 +167,25 @@ if (closeModel) {
       addNewAddressModel[index].classList.remove("show");
       overLay.classList.remove("show");
     });
-  })
-
+  });
 }
 window.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
-    addNewAddressModel.forEach(model => {
+    addNewAddressModel.forEach((model) => {
       model.classList.remove("show");
       overLay.classList.remove("show");
-    })
+    });
   }
 });
 
 // Filter Page
 let filterBtns = document.querySelectorAll(".btns .filter-btn");
 if (filterBtns) {
-  filterBtns.forEach(btn => {
+  filterBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
       btn.classList.toggle("active");
-
-    })
-  })
+    });
+  });
 }
 
 const rangeInput = document.querySelectorAll(".range-input input"),
